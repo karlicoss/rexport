646a0513a59a7fb7bc207305ef7e48cf8f4a7915
Dima Gerasimov <karlicoss@gmail.com>
2020-09-25 18:16:24 +0200

    Convert to a proper PIP package with setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_namespace_packages # type: ignore
+
+
+def main():
+    pkgs = find_namespace_packages('src')
+    pkg = min(pkgs)
+    return setup(
+        name=pkg,
+        zip_safe=False,
+        packages=pkgs,
+        package_dir={'': 'src'},
+        package_data={pkg: ['py.typed']},
+
+        install_requires=[
+            'pytz',
+            'praw', # Reddit API
+        ],
+        extras_require={
+            'testing': ['pytest'],
+            'linting': ['pytest', 'mypy'],
+        },
+    )
+
+
+if __name__ == '__main__':
+    main()
