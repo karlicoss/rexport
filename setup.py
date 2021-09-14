fedb332000e0f9b415209aa7076af79cb66678d0
Dima Gerasimov <karlicoss@gmail.com>
2021-09-14 23:57:41 +0100

    general: update project template

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
+# see https://github.com/karlicoss/pymplate for up-to-date reference
+
+
 from setuptools import setup, find_namespace_packages # type: ignore
 
 
-def main():
+def main() -> None:
+    # works with both ordinary and namespace packages
     pkgs = find_namespace_packages('src')
-    pkg = min(pkgs)
-    return setup(
+    pkg = min(pkgs) # lexicographically smallest is the correct one usually?
+    setup(
         name=pkg,
+        use_scm_version={
+            'version_scheme': 'python-simplified-semver',
+            'local_scheme': 'dirty-tag',
+        },
+        setup_requires=['setuptools_scm'],
+
+        # otherwise mypy won't work
+        # https://mypy.readthedocs.io/en/stable/installed_packages.html#making-pep-561-compatible-packages
         zip_safe=False,
+
         packages=pkgs,
         package_dir={'': 'src'},
+        # necessary so that package works with mypy
         package_data={pkg: ['py.typed']},
+
+        ## ^^^ this should be mostly automatic and not requiring any changes
 
         install_requires=[
             'pytz',
@@ -17,10 +33,11 @@ def main():
         ],
         extras_require={
             'testing': ['pytest'],
-            'linting': ['pytest', 'mypy'],
+            'linting': ['pytest', 'mypy', 'lxml'], # lxml for mypy coverage report
         },
     )
 
 
 if __name__ == '__main__':
     main()
+
